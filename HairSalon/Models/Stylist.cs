25e276612d2aad9ac32f447460e7ee75ee8f4bdc
Mike Donovan <mike@mikedonovan.dev>
2023-03-10 16:15:23 -0800

    Build basic components, most functionality working

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,15 @@
+
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string HireDate { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Specialty { get; set; }
+    public List<Client> Clients { get; set; }
+  }
+}
